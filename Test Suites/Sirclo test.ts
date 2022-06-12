0741936fb878f552884f96766b227dbf5f9ea780
ajirafael <ajirafael02@gmail.com>
2022-06-12 09:40:59 +0700

    sirclo

---
+++ Test Suites/Sirclo test.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sirclo test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e39fdfce-3211-4b5c-95e4-48125871a2b4</testSuiteGuid>
+   <testCaseLink>
+      <guid>fb746b28-90ef-4800-9c6e-b83abf6b9ad2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/belum login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>806b2fc6-cff5-48b1-9ea0-71cc2bbab91d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login gagal</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>032c650c-27fc-45dc-a959-285960a5fb31</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ae474bde-1571-4862-bac5-9fa1ba44e50d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Filter data gagal</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e0a24178-b9ce-47af-ae8d-994c131d9ba0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Filter data berhasil</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>777b876e-9b2e-4936-80ab-b01a9a5bcee0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
